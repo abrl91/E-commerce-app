a663ee7d36202b657e6c06fb1a3ce7084ffd571d
Amit Barletz <amitbarletz@outlook.com>
2021-05-21 17:07:44 +0300

    initialization

---
+++ index.js
@@ -0,0 +1,12 @@
+// configure express server
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Hi There!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
