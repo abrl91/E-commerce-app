5394f3f64abc46403597edf4d694d78a0a79ec9d
Amit Barletz <amitbarletz@outlook.com>
2021-05-21 17:54:00 +0300

    test commit

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hi There!');
+   res.send(`
+      <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="password confirmation" placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </formi>
+      </div>
+   `);
+});
+
+app.post('/', (req, res) => {
+   res.send('authenticated!');
 });
 
 const PORT = 3000;
